bc1b35b6f67626cb3112c74588797362c174a35c
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-12-06 12:44:58 +0300

    file.io practice

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as f:
+        return f.read()
+    
+write_file(file_name="time_file", file_content="time is run")
+append_file(file_name="time_file", append_content="time is stop")
+print(read_file("time_file"))
+
